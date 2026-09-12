cbf4dad457e2e37837da3ad33021b939ee2460e4
MakdumIbrohim <makdumibrohim3@gmail.com>
2026-09-12 12:18:47 +0700

    chore: set svg as default render output and bump to v1.3.1

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ from src.config import DEFAULT_SKIN, DEFAULT_THEME, DEFAULT_PADDLE_SKIN
 def main():
     username = sys.argv[1] if len(sys.argv) > 1 else os.getenv("GITHUB_ACTOR", "MakdumIbrohim")
     token = os.getenv("GITHUB_TOKEN", None)
-    output_path = sys.argv[2] if len(sys.argv) > 2 else "game.gif"
+    output_path = sys.argv[2] if len(sys.argv) > 2 else "game.svg"
     skin = sys.argv[3] if len(sys.argv) > 3 else os.getenv("BALL_SKIN", DEFAULT_SKIN)
     theme = sys.argv[4] if len(sys.argv) > 4 else os.getenv("THEME", DEFAULT_THEME)
     paddle_skin = sys.argv[5] if len(sys.argv) > 5 else os.getenv("PADDLE_SKIN", DEFAULT_PADDLE_SKIN)
